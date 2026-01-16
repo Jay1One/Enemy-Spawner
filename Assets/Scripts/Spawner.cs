709c27738e412f2b64842ee64617ec54da5bc283
Artem Rusakov <artem0rusakov@gmail.com>
2026-01-17 01:55:56 +0700

    fixes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -5,24 +6,33 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private float _spawnInterval;
-    [SerializeField] private Enemy enemyPrefab;
+    [SerializeField] private Enemy _enemyPrefab;
     
     private float _timer;
 
-    private void Update()
+    private void Start()
     {
-        _timer += Time.deltaTime;
-        
-        if (_timer >= _spawnInterval)
-        {
-            Transform randomSpawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
-            Enemy enemy = Instantiate(enemyPrefab, randomSpawnPoint.position, Quaternion.identity);
-            enemy.StartMoving(ChooseDirection());
-            
-            _timer-= _spawnInterval;
-        }
+        StartCoroutine(SpawnCoroutine());
     }
 
+    private IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            _timer += Time.deltaTime;
+        
+            if (_timer >= _spawnInterval)
+            {
+                Transform randomSpawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+                Enemy enemy = Instantiate(_enemyPrefab, randomSpawnPoint.position, Quaternion.identity);
+                enemy.StartMoving(ChooseDirection());
+            
+                _timer-= _spawnInterval;
+            }
+            yield return null;
+        }
+    }
+    
     private Vector2 ChooseDirection()
     {
         Vector2 randomDirection = Random.insideUnitCircle;
